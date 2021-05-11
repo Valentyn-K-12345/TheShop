811a93ad03eb0dc2b0203c5c946ad7245d64d8e4
Valentyn-K-12345 <valentyn.k12345@gmail.com>
2021-05-11 20:54:30 +0300

    add domain

---
+++ TheShop.Domain/Interfaces/IArticleRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TheShop.Domain.Models;
+
+namespace TheShop.Domain.Interfaces
+{
+    public interface IArticleRepository
+    {
+        Article GetArticle(int id);
+        Article AddArticle(Article article);
+        Article AddArticleSale(ArticleSale sale);
+    }
+}
---
+++ TheShop.Domain/Models/ArticleSale.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheShop.Domain.Models
+{
+    public class ArticleSale
+    {
+        public int ArticleId { get; set; }
+        public int BuyerId { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
