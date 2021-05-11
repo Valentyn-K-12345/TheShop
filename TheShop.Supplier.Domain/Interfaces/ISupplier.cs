d46f445f7d803cb3ddbd73c938ec8d2012165a77
Valentyn-K-12345 <valentyn.k12345@gmail.com>
2021-05-11 21:28:16 +0300

    Add Supliers

---
+++ TheShop.Supplier.Domain/Interfaces/ISupplier.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TheShop.Supplier.Domain.Models;
+
+namespace TheShop.Supplier.Domain.Interfaces
+{
+    interface ISupplier
+    {
+        bool ArticleInInventory(int id);
+        SupplierArticle GetArticle(int id);
+    }
+}
