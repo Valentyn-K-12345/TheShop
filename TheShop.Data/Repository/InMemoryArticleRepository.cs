c75cede41d432b746ac48fd768237670ee700cf2
Valentyn-K-12345 <valentyn.k12345@gmail.com>
2021-05-12 12:36:38 +0300

    adding tracing and checks to article repository

--- TheShop.Data/Repository/InMemoryArticleRepository.cs
+++ TheShop.Data/Repository/InMemoryArticleRepository.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TheShop.Core.Logger;
 using TheShop.Domain.Interfaces;
 using TheShop.Domain.Models;
 
@@ -13,14 +14,23 @@ namespace TheShop.Data.Repository
         private List<Article> _articles;
         private List<ArticleSale> _articaleSales;
 
-        public InMemoryArticleRepository()
+        private readonly ILogger _logger;
+
+        public InMemoryArticleRepository(ILogger logger)
         {
+            _logger = logger;
             _articles = new List<Article>();
             _articaleSales = new List<ArticleSale>();
         }
 
         public Article SaveArticle(Article article)
         {
+            if(article == null)
+            {
+                throw new ArgumentNullException(nameof(article));
+            }
+
+            _logger.Debug($" >> Saving article with Name: {article.Name}");
             //simple upsert
             if(article.Id == 0)
             {
@@ -32,17 +42,32 @@ namespace TheShop.Data.Repository
             }
             _articles.Add(article);
 
+            _logger.Debug($" << saved article with Name: {article.Name} and Id: {article.Id}");
+
+
             return article;
         }
 
         public ArticleSale AddArticleSale(ArticleSale sale)
         {
+            if (sale == null)
+            {
+                throw new ArgumentNullException(nameof(sale));
+            }
+
+            _logger.Debug($" >> Saving article sale with article id: {sale.ArticleId} and buyerId: {sale.BuyerId}");
+
             _articaleSales.Add(sale);
+
+            _logger.Debug($" << saved article sale with article id: {sale.ArticleId} and buyerId: {sale.BuyerId}");
+
             return sale;
         }
 
         public Article GetArticle(int id)
         {
+            _logger.Debug($" >> getting article with id: {id}");
+
             return _articles.FirstOrDefault(a => a.Id == id);
         }
     }
